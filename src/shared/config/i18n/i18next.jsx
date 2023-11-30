b7a196f2e48f21605ebdab51a55f3f93b98016f8
Myrhoiazov <denis.mirgoyazov@gmail.com>
2023-11-30 22:59:28 +0100

    [Init] project.

--- src/shared/config/i18n/i18next.jsx
+++ src/shared/config/i18n/i18next.jsx
@@ -3,10 +3,27 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import {initReactI18next} from 'react-i18next';
 
+import translationEN from 'locales/en/translation.json';
+import translationUA from 'locales/ua/translation.json';
+import translationPL from 'locales/pl/translation.json';
+
+const resources = {
+	en: {
+		translation: translationEN,
+	},
+	ua: {
+		translation: translationUA,
+	},
+	pl: {
+		translation: translationPL,
+	},
+};
+
 i18n.use(Backend)
 	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
+		resources,
 		fallbackLng: 'ua',
 		whitelist: ['ua', 'en', 'ru'],
 		debug: false,
